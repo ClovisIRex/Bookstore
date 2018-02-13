252d31fda79b4909cceb63058eaea5f8c59870bb
SolInvictus <livnytal.dev@gmail.com>
2018-02-13 10:32:12 +0200

    finished server, no tests w/mocha yet

--- routes/book.js
+++ routes/book.js
@@ -1,63 +1,67 @@
 const express = require('express');
 const router = express.Router();
-const Book = require('../helpers/bookLogic')
+const Book = require('../models/Book');
 const validate = require('../helpers/validate')
 const { ValidationError, AlreadyExistsError } = require('../errors/error')
 
 
-/* GET ALL BOOKS */
-router.get('/',async (req, res, next) => {
-  try {
-    let books = await Book.getBooks();
-    res.json(books);
-  } catch (error) {
-    next(error)
-  }
-});
+/* GET ALL BOOKS */ 
+router.get('/',async (req, res, next) => { 
+  try { 
+    let books = await Book.find(); 
+    res.json(books); 
+  } catch (error) { 
+    next(error) 
+  } 
+}); 
 
 
-/* GET SINGLE BOOK BY ID */
+/* GET SINGLE BOOK BY ID */ 
 router.get('/:id', async (req, res, next) => {
   try {
-    let book = await Book.getBookById(req.params.id);
-    res.json(book);
+    let books = await Book.findById(req.params.id)
+    res.json(books)
+  } catch (error) {
+    next(err)
+  }
+}); 
+
+/* CREATE BOOK */
+router.post('/', async (req, res, next) => {
+  try {
+    let book = await validate(req.body);
+
+    Book.find({'title': book.title}).exec( async (err, docs) => {
+      if (docs.length) {
+        next(new AlreadyExistsError());
+      } else {
+        let result = await Book.create(book);
+        res.json(result)
+      }
+    })
   } catch (error) {
     next(error)
   }
 });
 
-// /* CREATE BOOK */
-// router.post('/', async (req, res, next) => {
-//   try {
-//     let book = await validate(req.body);
+/* UPDATE BOOK */
+router.put('/:id', async (req, res, next) => {
+  try {
+    let result = await Book.findByIdAndUpdate(req.params.id,req.body)
+    res.json(result)
+  } catch (error) {
+    next(error)  
+  }
+});
 
-//     Book.find({'title': book.title}).exec( async (err, docs) => {
-//       if (docs.length) {
-//         next(new AlreadyExistsError());
-//       } else {
-//         let result = await Book.create(book);
-//         res.json(result)
-//       }
-//     })
-//   } catch (error) {
-//     next(error)
-//   }
-// });
-
-// /* UPDATE BOOK */
-// router.put('/:id', function(req, res, next) {
-//   Book.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
-//     if (err) return next(err);
-//     res.json(post);
-//   });
-// });
-
-// /* DELETE BOOK */
-// router.delete('/:id', function(req, res, next) {
-//   Book.findByIdAndRemove(req.params.id, req.body, function (err, post) {
-//     if (err) return next(err);
-//     res.json(post);
-//   });
-// });
+/* DELETE BOOK */
+router.delete('/:id', async (req, res, next) => {
+  try {
+    let result = await Book.findByIdAndRemove(req.params.id)
+    res.json(result)
+  } catch (error) {
+    next(err)
+  }
+});
 
 module.exports = router;
